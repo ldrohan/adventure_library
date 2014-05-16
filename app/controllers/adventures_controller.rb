26074ebc9790fdfd47b547ec0862138b32b8b444
ldrohan <drohanluke@gmail.com>
2014-05-16 11:57:20 -0700

    fixing new adventure bug

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -6,22 +6,24 @@ class AdventuresController < ApplicationController
       format.json { render :json => {:adventures => @adventures.as_json(except: [:id, :library_id], include: {:pages => {except: [:id, :adventure_id, :created_at, :updated_at]} })} }
     end
   end
+
+  def new
+    @adventure = Adventure.new
+  end
+
   def create
     @adventure = Adventure.new adventure_params
-    #BELOW IS maybe WRONG!!!!!!!!!!
+    #BELOW maybe wrong!!!!!!!!!
     @adventure.guid = SecureRandom.urlsafe_base64(10)
     if @adventure.save
-      redirect_to adventures_path
+      redirect_to new_adventure_page_path(@adventure)
     else
       flash[:errors] = @adventure.errors.full_messages
       render :edit
+      redirect_to :back
     end
-
-
-    def new
-      @adventure = Adventure.new
-    end
-
+  end
+   
     def show
       # id = params[:id]
       @adventure = Adventure.find(params[:id])
@@ -35,7 +37,7 @@ class AdventuresController < ApplicationController
 
     private
     def adventure_params
-      params.require(:adventure).permit(:title, :author, :url)
+      params.require(:adventure).permit(:title, :author)
     end
-  end
+  
 end
